91dd81d41b39850d8f6223beb3413df1281e96c6
Diego Andres Cabrera Rojas <diegoandresrojas2000@gmail.com>
2021-01-08 23:28:56 -0500

    refactor files privacy

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-require("dotenv").config({ path: ".env.development" });
+require("dotenv").config({ path: ".env" });
 
 const connectDB = async () => {
   try {
